4797ac73ea8026cdcea5e7bdf0d6ea856b99d1f0
Cram42 <cram42@users.noreply.github.com>
2022-05-05 18:43:53 +0800

    Comment out leftover debugging message

--- src/KnobAcceleration/KnobAcceleration.cpp
+++ src/KnobAcceleration/KnobAcceleration.cpp
@@ -22,6 +22,10 @@ float KnobAcceleration::get_acceleration(uint16_t time_span_ms) {
     float a_min = 1.0;                                  //   1.0
     float a_max = this->max_acceleration_multiplier;    //  10.0
 
+    // accel = ((t - t_slow) ^ 2 / (t_fast - t_slow) ^ 2) * (a_max - a_min) + a_min
+
+    float accel = pow(t - t_slow, 2) / pow(t_fast - t_slow, 2) * (a_max - a_min) + a_min;
+
     /*
     Serial.print("ACCEL | ");
     Serial.print("t: "); Serial.print(t); Serial.print(" ");
@@ -29,11 +33,9 @@ float KnobAcceleration::get_acceleration(uint16_t time_span_ms) {
     Serial.print("t_slow: "); Serial.print(t_slow); Serial.print(" ");
     Serial.print("a_min: "); Serial.print(a_min); Serial.print(" ");
     Serial.print("a_max: "); Serial.print(a_max); Serial.print(" ");
+    Serial.print("accel: "); Serial.print(accel); Serial.print(" ");
+    Serial.println();
     */
-    // accel = ((t - t_slow) ^ 2 / (t_fast - t_slow) ^ 2) * (a_max - a_min) + a_min
-
-    float accel = pow(t - t_slow, 2) / pow(t_fast - t_slow, 2) * (a_max - a_min) + a_min;
-    Serial.print("accel: "); Serial.print(accel); Serial.println(" ");
 
     return accel;
 }
